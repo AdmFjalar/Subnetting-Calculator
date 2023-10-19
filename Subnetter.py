f7349e73d9fad822ff4660e73fbdb4c4e24de9c2
Tom Morrison <thomas.m.morrison.101@gmail.com>
2023-10-19 15:06:17 +1300

    Added while loop

--- Subnetter.py
+++ Subnetter.py
@@ -51,28 +51,45 @@ def Main():
 
     maxSubnets = CalculateMaxSubnets(ip, cidr)                      # Calculates the maximum number of subnets for the given IP and CIDR
 
-    try:
-        hostsPerSubnet = []                                         # Initializes the list of hosts per subnet
-        for i in range(maxSubnets):                                 # Loops through the max number of subnets
-            maxHosts = (2 ** (32 - cidr))                           # Calculates the maximum number of hosts for the given IP and CIDR
-            addressesLeft = maxHosts - sum(hostsPerSubnet)          # Calculates the number of addresses left after subtracting the number of hosts for each subnet
-            print("\n-------------------------------------------")
-            print(f"----------------- SUBNET {i + 1} ----------------")
-            print("-------------------------------------------")
-            print(f"Total number of adresses left: {addressesLeft}")                                                            # Prints the number of addresses left in the range
-            print(f"Maximum number of hosts supported for subnet {i+1}: {2**math.floor(math.log2(addressesLeft)) - 2}")         # Prints the maximum number of hosts supported for the subnet
-            hostsPerSubnet.append(2**math.ceil(math.log2(int(input(f"Enter the number of hosts for this subnet: ")) + 2)))      # Gets user input and calculates the number of hosts for the subnet
-            if (hostsPerSubnet[i] > addressesLeft):                                                                             # If the number of hosts entered is greater than the number of addresses left, print error message and default to the max number of hosts supported
-                print(f"\nToo many hosts entered, will now default to {2**math.floor(math.log2(addressesLeft)) - 2} hosts.\n")  # Prints the number of hosts that will be used for the subnet
-                hostsPerSubnet[i] = 2**math.floor(math.log2(addressesLeft))
-            addressesLeft = maxHosts - sum(hostsPerSubnet)          # Calculates the number of addresses left after subtracting the number of hosts for each subnet
-            if (addressesLeft <= 0):                                # If the number of addresses left is less than or equal to 0, print message and break loop
-                print("Max hosts reached.")
-                break
-        hostsPerSubnet.sort(reverse=True)
-    except ValueError:
-        print("Invalid input data type.")
-        return
+#create a while loop here
+    doSubs=True
+    hostsPerSubnet = []                                             # Initializes the list of hosts per subnet (MOVED FROM TRY)
+    while doSubs:                                                       #loops as long as doSubs=True 
+        try:
+                                                  
+            for i in range(len(hostsPerSubnet), maxSubnets):                                 # Loops through the max number of subnets
+                maxHosts = (2 ** (32 - cidr))                           # Calculates the maximum number of hosts for the given IP and CIDR
+                addressesLeft = maxHosts - sum(hostsPerSubnet)          # Calculates the number of addresses left after subtracting the number of hosts for each subnet
+                print("\n-------------------------------------------")
+                print(f"----------------- SUBNET {i + 1} ----------------")
+                print("-------------------------------------------")
+                print(f"Total number of adresses left: {addressesLeft}")                                                            # Prints the number of addresses left in the range
+                print(f"Maximum number of hosts supported for subnet {i+1}: {2**math.floor(math.log2(addressesLeft)) - 2}")         # Prints the maximum number of hosts supported for the subnet
+                hostsPerSubnet.append(2**math.ceil(math.log2(int(input(f"Enter the number of hosts for this subnet: ")) + 2)))      # Gets user input and calculates the number of hosts for the subnet
+                
+                if (hostsPerSubnet[i] > addressesLeft):                                                                             # If the number of hosts entered is greater than the number of addresses left, print error message and default to the max number of hosts supported
+                    print(f"\nToo many hosts entered, will now default to {2**math.floor(math.log2(addressesLeft)) - 2} hosts.\n")  # Prints the number of hosts that will be used for the subnet
+                    hostsPerSubnet[i] = 2**math.floor(math.log2(addressesLeft))
+                addressesLeft = maxHosts - sum(hostsPerSubnet)          # Calculates the number of addresses left after subtracting the number of hosts for each subnet
+                if (addressesLeft <= 0):                                # If the number of addresses left is less than or equal to 0, print message and break loop
+                    print("Max hosts reached.")
+                    doSubs=False                                        #Sets doSubs to false and ends loop
+                    break
+                set_bool=input("Continue? Enter Y to continue or N to finish: ") #asks user if they want to keep making subnets or finish
+                if set_bool.lower()=='y':
+                    doSubs=True                                                  #continues loop
+                elif set_bool.lower()=='n':
+                    doSubs=False                                                 #ends loop
+                else:
+                    print(f'\nInvalid input, please enter Y or N: ')
+            hostsPerSubnet.sort(reverse=True)
+
+        
+        except ValueError:
+            print("Invalid input data type.")                                #breaks Try and continues loop if data error
+            doSubs=True
+            
+#end loop here
 
     subnets = CalculateSubnets(ip, cidr, hostsPerSubnet)    # Calculates the subnet masks for the given IP, CIDR, and hosts per subnet
 
