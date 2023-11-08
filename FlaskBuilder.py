6a68f8315f4ed2ddf4508d19405e8eb821120ea6
Dylan Green <dylan.green801@gmail.com>
2023-11-08 13:15:05 +1300

    Data population
    populating Jinja templates with data from a python script

--- FlaskBuilder.py
+++ FlaskBuilder.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 from Subnetter import *
+
 app = Flask(__name__)
 
 @app.route('/subnet-explanations')
 def HomeFunc():
-    return render_template("subnet-explanations.html", network_address = "192.168.0.0", cidr=24)
+    # Define the data you want to pass to the template
+    network_address = "192.168.0.0"
+    cidr = 24
+
+    # Pass the data to the template
+    return render_template("subnet-explanations.html", network_address=network_address, cidr=cidr)
+
 @app.route('/about')
 def AboutFunc():
     return "About"
