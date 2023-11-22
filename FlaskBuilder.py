4561f42281c811ae83c92a8732d980d9cc918b0d
Oliver Grönkrans <oliver@gronkrans.com>
2023-11-23 11:57:12 +1300

    Documented code
    * Added comments to FlaskBuilder.py
    * Added comments to thick sections in subnet-results.html
    * Fixed grammatical and spelling errors in aboutapp.html and aboutsubnets.html

--- FlaskBuilder.py
+++ FlaskBuilder.py
@@ -21,104 +21,145 @@ def HomeFunc():
 
 
 
+# Flask route for handling POST requests to generate a URL
 @app.route('/generate_url', methods=['POST'])
 def generate_url():
-    base_ip = request.form['base_ip']   
+    # Extracting data from the form submitted via POST
+    base_ip = request.form['base_ip']
     base_cidr = int(request.form['base_cidr'])
     hostsPerSubnet = request.form.get('hosts_per_subnet', [])
 
-    if (ValidateIPAndCIDR(base_ip, base_cidr) == False):
+    # Validate the provided IP and CIDR
+    if not ValidateIPAndCIDR(base_ip, base_cidr):
         flash("Invalid IP and/or CIDR!", 'error')
         return redirect('/')
 
+    # Convert the hostsPerSubnet string to a list of integers
     if hostsPerSubnet:
-        hostsPerSubnet = [ int(subnetHosts) for subnetHosts in hostsPerSubnet.split("-")]
+        hostsPerSubnet = [int(subnetHosts) for subnetHosts in hostsPerSubnet.split("-")]
+
+    # Initialize hostsPerSubnet as an empty list if it's not provided
     if hostsPerSubnet == "":
         hostsPerSubnet = []
+
+    # Extract the total number of hosts requested
     hosts = int(request.form.get('hosts', 0))
 
-    maxHosts = (2 ** (32 - base_cidr))      
-    
+    # Calculate the maximum number of hosts for the given CIDR
+    maxHosts = 2 ** (32 - base_cidr)
+
+    # Initialize a variable to track the hosts list for URL generation
     hostsList = "new"
+
+    # Process the hosts list based on user input
     if not hosts and hostsPerSubnet:
+        # If hosts are not provided but hostsPerSubnet is, use hostsPerSubnet as the list
         hostsList = "-".join([str(item) for item in hostsPerSubnet])
     elif hosts:
+        # If hosts are provided, calculate and adjust the hostsPerSubnet list
         addressesLeft = maxHosts - sum(hostsPerSubnet)
 
-        if addressesLeft > 0:    
-            hosts = 2**math.ceil(math.log2(hosts))
+        if addressesLeft > 0:
+            # Ensure hosts is a power of 2 and fits within the available addresses
+            hosts = 2 ** math.ceil(math.log2(hosts))
             if hosts > addressesLeft:
-                hosts = 2**math.floor(math.log2(addressesLeft))
+                hosts = 2 ** math.floor(math.log2(addressesLeft))
+
+            # Add the calculated hosts to the hostsPerSubnet list
             hostsPerSubnet.append(hosts)
-            if (not CheckSortedList(hostsPerSubnet)):
+
+            # Check and sort the list in reverse order
+            if not CheckSortedList(hostsPerSubnet):
                 hostsPerSubnet.sort(reverse=True)
                 flash("Re-ordered subnets to fit a larger subnet", 'warning')
+
+        # Update the hostsList variable with the modified hostsPerSubnet list
         hostsList = "-".join([str(item) for item in hostsPerSubnet])
 
+    # Generate the URL based on the provided parameters
     url = f"/subnet/{base_ip}/{base_cidr}/{hostsList}"
-    
+
+    # Redirect the user to the generated URL
     return redirect(url)
 
+
+
+# Flask route for displaying subnet information based on user input
 @app.route('/subnet/<base_ip>/<base_cidr>/<hosts>')
 def show_subnet(base_ip, base_cidr, hosts):
+    # Check if the hosts parameter is set to "new" and handle it accordingly
     if hosts == "new":
         hosts = []
     else:
-        # Get hosts list by splitting hosts_list 
+        # Get hosts list by splitting hosts parameter
         hosts = hosts.split("-")
-   
-    if (ValidateIPAndCIDR(base_ip, base_cidr) == False):
+
+    # Validate the provided IP and CIDR
+    if not ValidateIPAndCIDR(base_ip, base_cidr):
         flash("Invalid IP and/or CIDR!", 'error')
         return redirect('/')
 
-    maxHosts = (2 ** (32 - int(base_cidr)))
+    # Calculate the maximum number of hosts for the given CIDR
+    maxHosts = 2 ** (32 - int(base_cidr))
 
+    # Initialize an empty list for recalculated hosts
     recalculatedHosts = []
 
+    # Loop through provided hosts and recalculate them to the nearest power of 2
     for i in range(len(hosts)):
-        recalculatedHosts.append(2**math.ceil(math.log2(int(hosts[i]))))
-        if (len(subnetColors) < len(hosts)):     
+        recalculatedHosts.append(2 ** math.ceil(math.log2(int(hosts[i]))))
+
+        # Add a random color for each subnet (if needed)
+        if len(subnetColors) < len(hosts):
             subnetColors.append('#' + '%06x' % random.randint(0, 0xFFFFFF))
 
-    # Call subnetter functions to generate subnets    
+    # Call subnetter functions to generate subnets
     generatedSubnets = CalculateSubnets(base_ip, base_cidr, recalculatedHosts)
-   
+
+    # Get the number of generated subnets
     numberOfSubnets = len(generatedSubnets)
 
+    # Calculate the number of addresses left after allocating subnets
     addressesLeft = maxHosts - sum(recalculatedHosts)
 
-    if (addressesLeft < 0):
-        while(addressesLeft < 0):
+    # Ensure that the number of addresses left is non-negative
+    if addressesLeft < 0:
+        # If negative, adjust the hosts list and redirect to a new URL
+        while addressesLeft < 0:
             recalculatedHosts.pop()
             addressesLeft = maxHosts - sum(recalculatedHosts)
         hostsList = "-".join([str(item) for item in recalculatedHosts])
         url = f"/subnet/{base_ip}/{base_cidr}/{hostsList}"
         return redirect(url)
 
+    # Initialize CIDR available to the default value of 32
     cidrAvailable = 32
 
-    if (addressesLeft > 0):
+    # If there are still available addresses, calculate the CIDR from the remaining hosts
+    if addressesLeft > 0:
         cidrAvailable = CIDRFromHosts(addressesLeft)
-        if (2**cidrAvailable > addressesLeft):
+        # Adjust CIDR if the calculated value is greater than the actual addresses left
+        if 2 ** cidrAvailable > addressesLeft:
             cidrAvailable = 32 - math.floor(math.log2(int(addressesLeft)))
 
-    # calculate base network object and extract base broadcast
-    baseNetwork=CalculateNetwork(base_ip, base_cidr)
-    base_broadcast=baseNetwork.broadcast_address
+    # Calculate base network object and extract base broadcast address
+    baseNetwork = CalculateNetwork(base_ip, base_cidr)
+    base_broadcast = baseNetwork.broadcast_address
 
-    # Return results template
-
+    # Render the template with the calculated subnet information
     return render_template('subnet-results.html',
-                          base_ip=base_ip,   
-                          base_cidr=base_cidr,
-                          generatedSubnets=generatedSubnets,
-                          numberOfSubnets=numberOfSubnets,
-                          addressesLeft=addressesLeft,
-                          cidrAvailable=cidrAvailable,
-                          subnetColors=subnetColors,
-                          hostsPerSubnet=recalculatedHosts,
-                          base_broadcast=base_broadcast,
-                          actual_hosts=hosts)
+                           base_ip=base_ip,
+                           base_cidr=base_cidr,
+                           generatedSubnets=generatedSubnets,
+                           numberOfSubnets=numberOfSubnets,
+                           addressesLeft=addressesLeft,
+                           cidrAvailable=cidrAvailable,
+                           subnetColors=subnetColors,
+                           hostsPerSubnet=recalculatedHosts,
+                           base_broadcast=base_broadcast,
+                           actual_hosts=hosts)
+
+
 
 #Route to aboutapp child
 @app.route('/aboutapp')
